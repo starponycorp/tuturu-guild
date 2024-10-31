73744d711ea4027c37470520ab88908cff57adda
starponycorp <130343724+starponycorp@users.noreply.github.com>
2024-10-31 23:46:52 +0500

    Инициализация Spring приложения

---
+++ src/main/java/com/starpony/tuturu/TuturuApplication.java
@@ -0,0 +1,13 @@
+package com.starpony.tuturu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuturuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuturuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starpony/tuturu/TuturuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starpony.tuturu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuturuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
